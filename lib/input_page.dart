3b855a5fd41b766e2ee755ef3b763b5442bdee3b
IzzetMustafaoglu <izzetmustafaolgu00@gmail.com>
2023-09-17 09:05:50 +0300

    spor gunleri eklendi

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,6 +12,7 @@ class _MyHomePageState extends State<InputPage> {
   int _counter = 0;
   late bool seciliCinsiyet;
   double icilenSigara=3;
+  double sporGunu = 2;
 
   void _incrementCounter() {
     setState(() {
@@ -70,6 +71,29 @@ class _MyHomePageState extends State<InputPage> {
           ),
           Expanded(
             child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Haftada kaç gün spor yapıyorsunuz?",
+                    style: TextStyle(color: Colors.black54,fontSize: 20,fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    sporGunu.round().toString(),
+                    style: TextStyle(color: Colors.lightBlue,fontSize: 35,fontWeight: FontWeight.bold),),
+                  Slider(
+                      min: 0,
+                      max: 7,
+                      divisions: 7,
+                      value: sporGunu,
+                      onChanged: (double newValue){
+                        setState(() {
+                          sporGunu = newValue;
+                        });
+                      }
+                  ),
+                ],
+              ),
               margin: EdgeInsets.all(12.0),
               decoration: BoxDecoration(borderRadius: BorderRadius.circular(10.0),color: Colors.white,),
             ),
